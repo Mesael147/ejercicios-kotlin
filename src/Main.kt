35a88e50ce6bf5101f8b69ac609b4c492a9b6bcd
Daniel Medrano <danielsnatiagom@gmail.com>
2024-08-16 16:02:10 -0500

    ejercicio lanzar moneda

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,32 @@
+import kotlin.random.Random
+
 fun main() {
-    println("mi primer commit")
+
+    var cara= 1
+    var sello= 2
+    var lanzarMoneda: Int =0
+    var contadorSello: Int=0
+    var contadorCara: Int=0
+    while (lanzarMoneda <=19){
+        val caeMoneda: Int= Random.nextInt(1, 3)
+        if (caeMoneda==sello){
+        contadorSello++
+        }else if (caeMoneda==cara){
+        contadorCara++
+        }
+    lanzarMoneda++
+    }
+    if (contadorSello>contadorCara){
+        println("sello gana con")
+        println(contadorSello)
+    }else if (contadorCara>contadorSello){
+        println("cara gana con")
+        println(contadorCara)
+    }
+
+
+
+
+
+
 }
